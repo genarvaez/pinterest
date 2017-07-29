c04e2de28b4ddfb0b8263d22cb620486c812098a
genarvaez <ge_naravez@hotmail.com>
2017-07-29 18:48:08 -0400

    modal terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,50 @@
-
-data.filter(function(e,i){
+$(document).ready(function(){
+	$(".modal").hide();
 	
-	if(i< 20){
+	data.filter(function(e,i){ //FILTRO PRIMERAS 20IMG
+		if(i< 20){
+		$(".contents").append('<div class="card_img"><img src="../dist/img/'+ e.id + '.jpg" ><h2 class="title">'+ e.title+'</h2><p class="description">'+		
+			e.description+'</p><span class="usuario">'+e.user.charAt()+'</span><h4>'+e.user+'</h4><span class="hashtag">#'+e.hashtag+'</span></div>');
+		}
+		$(window).scroll(function(){
+	   	if(i>=20&&$(window).scrollTop() + $(window).height() == $(document).height()){
+	       $(".contents").html(""); //vacío el modal para que no se repliquen las imagenes
+	       data.forEach(function(e){
+	       		$(".contents").append('<div class="card_img"><img src="../dist/img/'+ e.id + '.jpg" class="imagen" ><h2 class="title">'+ e.title+'</h2><p class="description">'+		
+			e.description+'</p><span class="usuario">'+e.user.charAt()+'</span><h4>'+e.user+'</h4><span class="hashtag">#'+e.hashtag+'</span></div>');
+	       })
+	       $(".contents").css("height", "1000vh"); //AUMENTO ALTO DE PANTALLA PARA ACOMODAR LAS IMG A LA VENTANA
+	   	}
+		});
+	})
 
-	$(".contents").append('<div class="card_img"><img src="../dist/img/'+ e.id + '.jpg" ><h2 class="title">'+ e.title+'</h2><p class="description">'+		
-		e.description+'</p><span class="usuario">'+e.user.charAt()+'</span><h4>'+e.user+'</h4><span class="hashtag">#'+e.hashtag+'</span></div>');
+	//ABRE MODAL
+	$(".card_img").click(modal);
+
+	function modal(){
+		$(".modal").show()		
+		var titulo = $(this).children()[1];
+		//filtro el titulo de la imagen seleccionada con los elementos de la data
+		var selected = data.filter(function(e){
+			if(titulo.innerHTML == e.title){
+				return e;
+			}
+		})
+		//introduzco elementos a modal
+		var icons = $(".icons").clone();
+		var botonRojo = $(".save").clone();
+		$(".contenido").append('<h1>'+ selected[0].title + '</h1>');
+		$(".contenido").append('<img src="../dist/img/'+ selected[0].id + '.jpg">');
+		$(".contenido").append(icons,botonRojo);
+		$(".contenido").append('<div class="user"><span class="usuario">'+selected[0].user.charAt()+'</span><h4>'+selected[0].user+'</h4><span class="hashtag">#'+selected[0].hashtag+'</span></div><button class="read">Leerlo</button>');
+		$(".contenido").append('<p class="description">' + selected[0].description + '</p>');
+
+		//CIERRE DE MODAL
+		$(".modal i").click(function(){
+		$(".modal").hide();
+		$(".contenido").html("");
+		})
 	}
-	$(window).scroll(function(){
-   	if(i>=20&&$(window).scrollTop() + $(window).height() == $(document).height()){
-       $(".contents").html("");
-       data.forEach(function(e){
-       		$(".contents").append('<div class="card_img"><img src="../dist/img/'+ e.id + '.jpg" ><h2 class="title">'+ e.title+'</h2><p class="description">'+		
-		e.description+'</p><span class="usuario">'+e.user.charAt()+'</span><h4>'+e.user+'</h4><span class="hashtag">#'+e.hashtag+'</span></div>');
-       })
-       $(".contents").css("height", "1000vh");
-   	}
-});
-	
+
 })
+

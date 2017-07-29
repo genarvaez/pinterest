afacc7d2f972df63af05b832d005f8cd4f981ec5
genarvaez <ge_naravez@hotmail.com>
2017-07-29 00:24:41 -0400

    todos los pines impresos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,6 @@
-console.log(data)
+
+data.forEach(function(e,i){
+	console.log(e)
+	$(".contents").append('<div class="card_img"><img src="../dist/img/'+ e.id + '.jpg" ><h2 class="title">'+ e.title+'</h2><p class="description">'+		
+		e.description+'</p><span class="usuario">'+e.user.charAt()+'</span><h4>'+e.user+'</h4><span class="hashtag">#'+e.hashtag+'</span></div>');
+})

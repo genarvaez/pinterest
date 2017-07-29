ee06401444a235ae4216d394245b6fe1ea2220b1
genarvaez <ge_naravez@hotmail.com>
2017-07-29 01:54:31 -0400

    carga de elementos al final de scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,20 @@
 
-data.forEach(function(e,i){
-	console.log(e)
+data.filter(function(e,i){
+	
+	if(i< 20){
+
 	$(".contents").append('<div class="card_img"><img src="../dist/img/'+ e.id + '.jpg" ><h2 class="title">'+ e.title+'</h2><p class="description">'+		
 		e.description+'</p><span class="usuario">'+e.user.charAt()+'</span><h4>'+e.user+'</h4><span class="hashtag">#'+e.hashtag+'</span></div>');
+	}
+	$(window).scroll(function(){
+   	if(i>=20&&$(window).scrollTop() + $(window).height() == $(document).height()){
+       $(".contents").html("");
+       data.forEach(function(e){
+       		$(".contents").append('<div class="card_img"><img src="../dist/img/'+ e.id + '.jpg" ><h2 class="title">'+ e.title+'</h2><p class="description">'+		
+		e.description+'</p><span class="usuario">'+e.user.charAt()+'</span><h4>'+e.user+'</h4><span class="hashtag">#'+e.hashtag+'</span></div>');
+       })
+       $(".contents").css("height", "1000vh");
+   	}
+});
+	
 })

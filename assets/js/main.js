70563013295e9c75cdaff21cacc13a368bf7b07f
genarvaez <ge_naravez@hotmail.com>
2017-07-29 23:24:15 -0400

    corrigiendo error en img

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
 	$(".modal").hide();
 	data.forEach(function(e){
-		$(".navigator ul").append('<li><img src="../dist/img/'+ e.id + '.jpg"><a href="#">'+e.hashtag+'</a></li>');
+		$(".navigator ul").append('<li><img src="../assets/img/'+ e.id + '.jpg"><a href="#">'+e.hashtag+'</a></li>');
 	})
 	data.filter(function(e,i){ //FILTRO PRIMERAS 20IMG
 		if(i< 20){
-		$(".contents").append('<div class="card_img"><img src="../dist/img/'+ e.id + '.jpg" ><h2 class="title">'+ e.title+'</h2><p class="description">'+		
+		$(".contents").append('<div class="card_img"><img src="../assets/img/'+ e.id + '.jpg" ><h2 class="title">'+ e.title+'</h2><p class="description">'+		
 			e.description+'</p><span class="usuario">'+e.user.charAt()+'</span><h4>'+e.user+'</h4><span class="hashtag">#'+e.hashtag+'</span></div>');
 		}
 		$(window).scroll(function(){
 	   	if(i>=20&&$(window).scrollTop() + $(window).height() == $(document).height()){
 	       $(".contents").html(""); //vacío el modal para que no se repliquen las imagenes
 	       data.forEach(function(e){
-	       		$(".contents").append('<div class="card_img"><img src="../dist/img/'+ e.id + '.jpg" class="imagen" ><h2 class="title">'+ e.title+'</h2><p class="description">'+		
+	       		$(".contents").append('<div class="card_img"><img src="../assets/img/'+ e.id + '.jpg" class="imagen" ><h2 class="title">'+ e.title+'</h2><p class="description">'+		
 			e.description+'</p><span class="usuario">'+e.user.charAt()+'</span><h4>'+e.user+'</h4><span class="hashtag">#'+e.hashtag+'</span></div>');
 	       })
 	       $(".contents").css("height", "900vh"); //AUMENTO ALTO DE PANTALLA PARA ACOMODAR LAS IMG A LA VENTANA
@@ -36,7 +36,7 @@ $(document).ready(function(){
 		var icons = $(".icons").clone();
 		var botonRojo = $(".save").clone();
 		$(".contenido").append('<h1>'+ selected[0].title + '</h1>');
-		$(".contenido").append('<img src="../dist/img/'+ selected[0].id + '.jpg">');
+		$(".contenido").append('<img src="../assets/img/'+ selected[0].id + '.jpg">');
 		$(".contenido").append(icons,botonRojo);
 		$(".contenido").append('<div class="user"><span class="usuario">'+selected[0].user.charAt()+'</span><h4>'+selected[0].user+'</h4><span class="hashtag">#'+selected[0].hashtag+'</span></div><button class="read">Leerlo</button>');
 		$(".contenido").append('<p class="description">' + selected[0].description + '</p>');
